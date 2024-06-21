16978ad29e10229259dfec31c690e5abaf524d8d
Jiayang Dong <jiayangdong@dhcp-206-87-134-92.ubcsecure.wireless.ubc.ca>
2024-06-21 14:26:36 -0700

    Jun 21st

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,16 @@
+let javaScriptIsFun = true;
+console.log(typeof true);
+console.log(typeof 23);
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1992;
+
+console.log(typeof year);
+
+console.log(typeof null);
